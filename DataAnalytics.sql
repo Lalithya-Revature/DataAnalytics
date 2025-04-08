58f60d3ecc1deb3075bac0defb95decbe33cd98d
Lalithya-7602 <lalithyakalluru@gmail.com>
2025-04-08 16:21:31 +0530

    Add files via upload

---
+++ DataAnalytics.sql
@@ -0,0 +1,19 @@
+-- create database DataAnalytics ;
+-- use DataAnalytics;
+-- create TABLE Persons (
+-- 		PersonID int,
+--         LastName varchar(255),
+--         FirstName varchar(255),
+--         Address varchar(255),
+--         City varchar(255)
+-- );
+--         
+--         
+-- INSERT INTO persons values(1, "Lalithya", "Kalluru", "Hostel", "Chennai"); 
+-- INSERT INTO persons values(2, "Lavanya", "Kalluru", "Home", "Nellore");
+-- INSERT INTO persons values(3, "Hema", "Kalluru", "Hostel", "Hyderabad");
+-- SELECT * FROM PERSONS;
+
+-- DELETE FROM persons WHERE PersonID=3;
+-- select * from persons;
+DELETE FROM PERSONS WHERE PERSONID = 3;
